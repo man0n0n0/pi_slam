c498f9674e8a34aef0c3678365662c2eb64cddda
camporini@protonmail.com <manoah.camporini@erg.school>
2025-09-22 18:13:37 +0200

    finetuning

--- software/main.py
+++ software/main.py
@@ -76,7 +76,7 @@ MAX_MESURED_DISTANCE = 40   # in dm
 K_SPEED = 30 # max speed for exponential function
 BACKWARD_SPEED = -20
 STEEPNESS_SPEED = 10 # Smaller steepness (e.g., 5) = faster acceleration, reaches max speed sooner // larger steepness gentler acceleration, more gradual speed increase
-MIN_READINGS_FRONT = 5  # Minimum readings in front sector to be valid ([# )Global variables for artifact filtering)
+MIN_READINGS_FRONT = 15  # Minimum readings in front sector to be valid ([# )Global variables for artifact filtering)
 
 # ==============================
 # Serial Connection to LiDAR
