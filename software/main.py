39cf98a0ef2c565358ca8a37ff124edca8dc3472
camporini@protonmail.com <manoah.camporini@erg.school>
2025-09-22 20:16:37 +0200

    esc fix : reverse sequence

--- software/main.py
+++ software/main.py
@@ -174,7 +174,7 @@ def the_callback(angles, distances):
 
     if FRONT_OBJECT:
         set_speed(BACKWARD_SPEED)
-        time.sleep(0.1)
+        time.sleep(0.5)
     else :
         # Speed based on a exponential functin that tend to max speed (k_value)
         set_speed(K_SPEED * (1 - math.exp(-MAX_DISTANCE/STEEPNESS_SPEED)))
