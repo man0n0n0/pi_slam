cf1425104d1a15a3d7689557e40dd952862a05d1
camporini@protonmail.com <manoah.camporini@erg.school>
2025-09-23 18:33:37 +0200

    logic

--- software/main.py
+++ software/main.py
@@ -164,12 +164,13 @@ def the_callback(angles, distances):
                 #TODO skip current_steering_angle -> turn directly to the desired angle
                 OBSTACLE_DETECTED = False # got back to normal 
 
-    # Update steering
-    if current_steering_angle is not None:
-        steering_degrees = math.degrees(current_steering_angle) if current_steering_angle <= math.pi else math.degrees(current_steering_angle - 2*math.pi)
-        TURN_ANGLE = TURN_ANGLE*0.7 + steering_degrees*0.3
-    
-    set_steering(TURN_ANGLE)
+    # # Update steering
+    # if current_steering_angle is not None:
+    #     steering_degrees = math.degrees(current_steering_angle) if current_steering_angle <= math.pi else math.degrees(current_steering_angle - 2*math.pi)
+    #     TURN_ANGLE = TURN_ANGLE*0.7 + steering_degrees*0.3
+    #set_steering(TURN_ANGLE)
+
+    set_steering(math.degre(steering_degrees))
 
     # Set speed & simple escape logic
     if CLOSE_OBJECT:
